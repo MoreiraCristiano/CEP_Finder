d5b5604b9a8cfeddb7f46073d66fc33815c20eb4
Cristiano Moreira <moreiracristiano702@gmail.com>
2022-04-01 20:16:23 -0300

    Inclusao do __name__= '__main__'

--- server/app.py
+++ server/app.py
@@ -1,15 +1,13 @@
+import requests
+import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import requests
 
 
 app = FastAPI()
-
-
 app.mount("/static", StaticFiles(directory="../static"), name="static")
-
 templates = Jinja2Templates(directory="../templates")
 
 
@@ -37,3 +35,7 @@ async def find_cep(request: Request, cep: int):
             "district": district,            
         }
     )
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
